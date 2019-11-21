bbd4e4ba6be05eac3e27611f8207a0c49172223d
rohitsabbanwad <rohitssabbanwad@gmail.com>
2019-11-21 10:32:18 +0530

    added 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int i, j;
+	int a[100];
+	for(i = 1;i <= 5;i++){
+		for(j = 0;j < i;j++){
+			printf("*\t");
+		}
+		printf("\n");
+	}
+	if(x == 0){
+	
+	}
+	return 0;
+}
